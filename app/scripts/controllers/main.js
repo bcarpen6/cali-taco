9da2a650f09e5e9faaa5368f257f23369d475214
Brett Carpenter <bcarpenter@thrillhouse.local>
2014-08-26 19:01:46 -0400

    yo app

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('caliTacoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
